8191e859aeb263c47d6a98d7cff78f0726227368
Monir Hossain <monir198@gmail.com>
2023-10-03 06:08:35 +0600

    added payment service

---
+++ paymentService/src/main/java/com/example/monir/paymentService/services/PaymentService.java
@@ -0,0 +1,10 @@
+package com.example.monir.paymentService.services;
+
+import com.example.monir.paymentService.model.PaymentRequest;
+import com.example.monir.paymentService.model.PaymentResponse;
+
+public interface PaymentService {
+    long doPayment(PaymentRequest paymentRequest);
+
+//    PaymentResponse getPaymentDetailsByOrderId(String orderId);
+}
