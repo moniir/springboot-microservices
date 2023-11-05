a2253df9dcf2a4efd72a9910e3a5dfded6ebea95
Monir <monir198@gmail.com>
2023-11-05 16:50:02 +0600

    Added circuit breaker functionality

---
+++ CloudGateway/src/main/java/com/example/monir/CloudGateway/controller/FallbackController.java
@@ -0,0 +1,21 @@
+package com.example.monir.CloudGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/orderServiceFallback")
+    public String orderServiceFallback(){
+        return "Order service is down!!";
+    }
+    @GetMapping("/paymentServiceFallback")
+    public String paymentServiceFallback(){
+        return "Payment service is down!!";
+    }
+    @GetMapping("/productServiceFallback")
+    public String productServiceFallback(){
+        return "Product service is down!!";
+    }
+}
